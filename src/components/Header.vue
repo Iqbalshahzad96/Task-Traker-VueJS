de329e1fbb2d6c130390d5b3f0adf55411cd48c8
iqbalshahzad96 <iqbalshahzad1996@gmail.com>
2021-11-23 17:35:27 +0500

    JSON-Server Setup, Refactoring to use the Backend, Json Apis and method

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,6 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <h3>{{ count }}</h3>
         <Button 
         @btn-click="$emit('toggle-add-task')"
         :text="showAddTask ? 'Close' : 'Add Task'"
@@ -18,18 +17,8 @@ export default{
         title: String,
         showAddTask: Boolean,
     },
-    data() {
-        return {
-            count : 0
-        }
-    },
     components: {
         Button,
-    },
-    methods: {
-        addTask() {
-            this.count += 1
-        }
     },
 }
 </script>
